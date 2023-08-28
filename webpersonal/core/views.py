70fec6417a1c35736d579f2cff8cfb825ca5863d
AlexYisus <alextaraguay20@gamil.com>
2023-08-28 17:25:34 -0500

    taller1

---
+++ webpersonal/core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+htmlBase = """"
+  
+"""
+def home(request):
+    return render(request, "core/home.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def shop(request):
+
+    return render(request, "core/shop.html")
+
+
+def shopsingle(request):
+
+    return render(request, "core/shopsingle.html")
+
+
+
+def contact(request):
+
+    return render(request, "core/contact.html")
